95a241356f64fc5225f90c8871f2e539572560b9
devIslam <dev.islam@gmail.com>
2021-03-14 14:08:44 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        enum X { a, b, c }
+
+        struct testStruct
+        {
+            private int a;
+            public int A
+            {
+                set { a = value; }
+                get { return a; }
+            }
+
+            public void M1()
+            {
+                Console.WriteLine("struct method");
+            }
+        }
+
+        class testClass
+        {
+            public const int a =3;
+            public string b;
+            public int c;
+            public testClass(int _a,string _b)
+            {
+                b = _b;
+            }
+
+            public testClass(int A,string B,int C):this(A,B)
+            {
+                c = C;
+            }
+        }
+        static void Main(string[] args)
+        {
+            const int xy = 99;
+            Console.WriteLine(xy.ToString());
+            testClass tc = new testClass(2, "vv", 4);
+          
+            Console.WriteLine(tc.b.ToString() + tc.c.ToString());
+            StringBuilder strbldr = new StringBuilder("ali");
+            Console.WriteLine(strbldr.ToString());
+            strbldr.Clear();
+            strbldr.Append("aaaa");
+            Console.WriteLine(strbldr.ToString());
+            string xx = "aaa";
+            if (xx is string)
+                Console.WriteLine("sss");
+
+        }
+    }
+}
