e0cab665fe065866fcc77661e421593fb9517573
devIslam <dev.islam@gmail.com>
2021-03-14 14:46:35 +0200

    on new branch changes

--- Program.cs
+++ Program.cs
@@ -40,6 +40,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             //3rd Comment from repote 
+            //on new branch
             const int xy = 99;
             Console.WriteLine(xy.ToString());
             testClass tc = new testClass(2, "vv", 4);
